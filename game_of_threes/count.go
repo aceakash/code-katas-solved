e7800e67ec33ff135dc658019d744c76b6b9099f
Akash K <akashkurdekar@gmail.com>
2022-05-12 21:38:01 +0100

    game of threes: refactor

--- game_of_threes/count.go
+++ game_of_threes/count.go
@@ -1,25 +1,19 @@
 package game_of_threes
 
 func Count(num int) []int {
-	nums := []int{}
-	nums = append(nums, num)
+	nums := []int{num}
 	for num != 1 {
-		if num%3 == 0 {
-			num = num / 3
+		if num%3 == 2 {
+			num += 1
 			nums = append(nums, num)
-			continue
 		}
 		if num%3 == 1 {
 			num -= 1
 			nums = append(nums, num)
-			num = num / 3
-			nums = append(nums, num)
-			continue
 		}
-		num += 1
-		nums = append(nums, num)
 		num = num / 3
 		nums = append(nums, num)
+		continue
 	}
 	return nums
 }
